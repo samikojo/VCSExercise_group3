4c0a86fd302bde064bfcd39745f70ef0ef03dcc0
unknown <c6maberg@KC411B14.tamk.ad>
2017-10-12 10:25:31 +0300

    Lul jump commit I think

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -4,8 +4,39 @@ namespace TAMK.VCSExample
 {
 	public class Mover : MonoBehaviour
 	{
+		float yVelocity;
+		bool grounded;
+
 		void Update()
 		{
+			if (transform.position.y == 0.5) {
+				grounded = true;
+				yVelocity = 0;
+			} else {
+				grounded = false;
+			}
+
+			if (!grounded) {
+				yVelocity -= 0.1f;
+			}
+
+			if (transform.position.y < 0.5f) {
+				transform.position = new Vector3 (transform.position.x, 0.5f, transform.position.z);
+				Debug.Log ("AAA");
+				yVelocity = 0;
+			}
+
+			if(Input.GetButtonDown("Jump") && grounded){
+				Debug.Log ("Jump got, yVelocity is " + yVelocity);
+				yVelocity = 1;
+			}
+
+			if (yVelocity < -1) {
+				yVelocity = -1;
+			}
+
+
+			transform.Translate (new Vector3 (0, yVelocity, 0));
 		}
 	}
 }
