0bf0e95c1b6911c69621c2f0f4c042b5c7f9812b
Valtteri <valtteri.hasia@cs.tamk.fi>
2017-10-12 10:17:31 +0300

    Moving

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -4,8 +4,26 @@ namespace TAMK.VCSExample
 {
 	public class Mover : MonoBehaviour
 	{
+
+		// Movement speed
+		[SerializeField]
+		private float speed;
+
 		void Update()
 		{
+			if (Input.GetKey (KeyCode.W)) {
+				transform.Translate (Vector3.forward * Time.deltaTime * speed);
+			} else if (Input.GetKey(KeyCode.S)) {
+				transform.Translate (Vector3.back * Time.deltaTime * speed);
+			}
+
+			if (Input.GetKey (KeyCode.A)) {
+				transform.Translate (Vector3.left * Time.deltaTime * speed);
+			} else if (Input.GetKey(KeyCode.D)) {
+				transform.Translate (Vector3.right * Time.deltaTime * speed);
+			}
+
+
 		}
 	}
 }
