7bb5aedb93655d9fb248269771dda346f408edd9
unknown <c4arodri@KC411B15.tamk.ad>
2017-10-12 10:13:53 +0300

    rotation

--- Assets/Code/Mover.cs
+++ Assets/Code/Mover.cs
@@ -4,8 +4,23 @@ namespace TAMK.VCSExample
 {
 	public class Mover : MonoBehaviour
 	{
+       [SerializeField]
+       float rotationSpeed = 90f;
+
+
 		void Update()
 		{
+            Rotate();
 		}
+
+        void Rotate()
+        {
+            if(Input.GetButton("Rotation"))
+            {
+                float rotation = Input.GetAxis("Rotation") * rotationSpeed;
+                transform.RotateAround(transform.position, transform.up, Time.deltaTime * rotation);
+            }
+            
+        }
 	}
 }
